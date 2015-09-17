82d4be24e5faf48ce5b1dcad656055cb9b4261d9
Thomas R. Koll <tomk32@gmail.com>
2015-09-17 10:58:16 +0200

    Make the fridge less noisy
    * Compare the weight with the last one and only publish if it has significantly changed
    * If it hasn't changed publish anyways every 30 seconds

--- firmware/fridge.ino
+++ firmware/fridge.ino
@@ -15,6 +15,11 @@ int zero_factor = 8458217;
 float calibration_factor = 11600;
 
 HX711 scale(DOUT, CLK);
+
+static float last_weight = 0.0;
+static unsigned long int last_published = 0;
+static unsigned int publish_interval = 30000; // ten seconds
+
 ESP8266WiFiMulti wifiMulti;
 
 IPAddress MQTTserver(158, 255, 212, 248);
@@ -60,7 +65,13 @@ void loop() {
   if (client.connected()) {
     client.loop();
     scale.set_scale(calibration_factor);
-    client.publish("devlol/h19/fridge/rawsamples", (String)scale.get_units());
+    static float weight = scale.get_units()
+    // only trigger after a longer period or if the weight change is greater than 10 grams
+    if (millis() - last_published > publish_interval || abs(weight - last_weight) > 0.01) {
+      client.publish("devlol/h19/fridge/rawsamples", (String) weight);
+      last_weight = weight;
+      last_published = millis();
+    }
   } else {
     if (client.connect("fridge", "devlol/h19/fridge/online", 0, true, "false")) {
       client.publish("devlol/h19/fridge/online", "true", true);
